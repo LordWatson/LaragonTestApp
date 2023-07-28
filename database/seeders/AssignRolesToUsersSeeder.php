d740540d88bb85b960732616ab4ba5634fee203c
LordWatson <alexander.watson.work@gmail.com>
2023-07-28 10:41:22 +0100

    created and seeded users, roles and permissions

---
+++ database/seeders/AssignRolesToUsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AssignRolesToUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $admin = User::where('email', 'admin@admin.com')->first();
+        $admin->assignRole('admin');
+
+        $users = User::whereNot('email', 'admin@admin.com')->get();
+
+        foreach($users as $user){
+            $user->assignRole('user');
+        }
+    }
+}
