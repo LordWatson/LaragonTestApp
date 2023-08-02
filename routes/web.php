79e337948c3aa7b9a0393e23799312a604a9f2f6
LordWatson <alexander.watson.work@gmail.com>
2023-08-02 17:54:04 +0100

    users routes permission

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('users', \App\Http\Controllers\UserController::class);
+    Route::resource('users', \App\Http\Controllers\UserController::class)->middleware('role:admin');
 });
 
 require __DIR__.'/auth.php';
